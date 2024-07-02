6ecd3b8db9ab764a21e6b65635912d4dc0288bf6
WinnieAtieno <winnieotieno773@gmail.com>
2024-07-03 00:57:53 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers = (arr)=>{
+   const newArr = [...arr] // make copy of array
+   return  newArr.slice(0,2)
+}
+
+const returnLastTwoDrivers = (arr) =>{
+    return arr.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+let result = selectingDrivers.map(select => console.log(select))
+
+afterEach(function() {
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']; // Reset drivers array after each test
+  });
+
+  function createFareMultiplier(num){
+
+    return (function (){
+       return  num * 5
+    })
+  }
+  function fareDoubler(fare){
+    return fare * 2;
+  }
+  function fareTripler(fare){
+    return fare * 3;
+  }
+  function selectDifferentDrivers(arrayOfDrivers,func){
+    
+   return  func(arrayOfDrivers)
+  
+   
+  }
+
+
+
